8ebb95cd22a7186983d3cb702780c9ceeee147bf
Devakinandan K <nandandevaki6@gmail.com>
2026-02-19 23:55:45 +0530

    small console change

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,36 @@
-import {WebSocketServer, WebSocket} from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port: 8080});
+const wss = new WebSocketServer({ port: 8080 });
 
-interface User{
-    socket: WebSocket,
-    room: string
+interface User {
+  socket: WebSocket;
+  room: string;
 }
 
 let AllUsers: User[] = [];
 
-wss.on("connection",function(socket){
-    socket.on("message", (message)=>{
-        //@ts-ignore
-        console.log("user has joined the chat",AllUsers.length+1);
-        let parsedMessage = JSON.parse(message.toString());
-        if(parsedMessage.type == "join"){
-            AllUsers.push({
-                socket,
-                room: parsedMessage.payload.roomId
-            })
-        }
-        if(parsedMessage.type == "chat"){
-            //roomTypeofInCoReqSocket
-            let currRoomType = AllUsers.find(x => socket == x.socket)?.room;
+wss.on("connection", function (socket) {
+  socket.on("message", (message) => {
+    //@ts-ignore
+    let parsedMessage = JSON.parse(message.toString());
+    if (parsedMessage.type == "join") {
+      AllUsers.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+      });
+      console.log(
+        `User joined room "${parsedMessage.payload.roomId}". Total users: ${AllUsers.length}`,
+      );
+    }
+    if (parsedMessage.type == "chat") {
+      //roomTypeofInCoReqSocket
+      let currRoomType = AllUsers.find((x) => socket == x.socket)?.room;
 
-            for(let i = 0; i < AllUsers.length; i++){
-                if(AllUsers[i]?.room == currRoomType){
-                    AllUsers[i]?.socket.send(parsedMessage.payload.message);
-                }
-            }
+      for (let i = 0; i < AllUsers.length; i++) {
+        if (AllUsers[i]?.room == currRoomType) {
+          AllUsers[i]?.socket.send(parsedMessage.payload.message);
         }
-    })
-})
+      }
+    }
+  });
+});
